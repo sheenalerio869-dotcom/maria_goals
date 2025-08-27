5e5f2d5ce14f7a853ee7a620bbe637eba84eb1fd
“sheenalerio869-dotcom” <sheenalerio869@gmail.com>
2025-08-27 20:54:00 +0800

    Firebase Configuration

---
+++ app/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrModTcPAO4AztgmVahhqfvLpv8TP3J1I",
+  authDomain: "maria-goals.firebaseapp.com",
+  projectId: "maria-goals",
+  storageBucket: "maria-goals.firebasestorage.app",
+  messagingSenderId: "843079989093",
+  appId: "1:843079989093:web:ebde9707051f2bc49e938c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
