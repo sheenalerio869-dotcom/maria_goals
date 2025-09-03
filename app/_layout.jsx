4068777f7e7869a889466374cbeb9c133e014287
“sheenalerio869-dotcom” <sheenalerio869@gmail.com>
2025-09-03 23:45:38 +0800

    Create goals

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,8 +2,9 @@ import { Stack } from "expo-router"
 import { StatusBar } from "expo-status-bar"
 
 export default function RootLayout() {
+
   return (
-    <>
+  <>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="goals" options={{ headerShown: false }} />
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBrModTcPAO4AztgmVahhqfvLpv8TP3J1I",
   authDomain: "maria-goals.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:843079989093:web:ebde9707051f2bc49e938c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
